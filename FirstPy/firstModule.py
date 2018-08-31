7b3675440a685857f6b1338781b0643768308202
Moussab-orabi <moussab.orabi@rosenberger.com>
2018-08-31 16:15:35 +0200

    first commit

---
+++ FirstPy/firstModule.py
@@ -0,0 +1,22 @@
+'''
+Created on 31 Aug 2018
+
+@author: M_Orabi
+'''
+
+print("hello world")
+# one line comment
+"""
+this is block Comment
+till here it is still comment
+
+"""
+
+x= "hello"
+print(x.capitalize())
+
+n= int(input("Number?"))
+if n<=10:
+    print(n+10)
+else:
+    print(n+5)
